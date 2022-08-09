4b73d791c5ef606f8af011bf2c40f497ede90ab6
EmDeeTee <m12@onet.com.pl>
2022-08-09 20:25:04 +0200

    Search engine integration

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFile>
+#include <QTextStream>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -17,6 +20,21 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_lineEdit_editingFinished()
 {
-    ui->webEngineView->load(QUrl(ui->lineEdit->text()));
+    QString query = ui->lineEdit->text();
+    const QString filename = "BrowserHistory.txt";
+    ui->webEngineView->load(QUrl("https://duckduckgo.com/?q=" + query));
+
+    QFile f(filename); // Nobody told me how to append text, so I will do it in the Michau wae.
+        if (!f.open(QFile::ReadOnly | QFile::Text)) return;
+        QTextStream in(&f);
+        QString oldText = in.readAll();
+
+
+        QFile file(filename);
+        if (file.open(QIODevice::ReadWrite)) {
+            QTextStream stream(&file);
+            stream << query << Qt::endl << oldText;
+            stream.flush();
+        }
 }
 
